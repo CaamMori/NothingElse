d61b5fb4c2039247aa1779af8d76fcdf912ef984
CaamMori <caamleaf@gmail.com>
2026-08-08 07:57:46 +0800

    feat(data): add AppDatabase

---
+++ app/src/main/java/com/caam/nothingelse/data/AppDatabase.kt
@@ -0,0 +1,9 @@
+package com.caam.nothingelse.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Note::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun noteDao(): NoteDao
+}
