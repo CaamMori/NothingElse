5ebe30886e7251ca2fe0a90d9fa5f12ebc1e88a7
CaamMori <caamleaf@gmail.com>
2026-08-09 00:37:46 +0800

    fix(editor): import spacer layout

--- app/src/main/java/com/caam/nothingelse/ui/EditNoteScreen.kt
+++ app/src/main/java/com/caam/nothingelse/ui/EditNoteScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Divider
