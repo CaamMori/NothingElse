d1b6118a43b68d8dcf02f5b06ee8c8aa1b5e92de
CaamMori <caamleaf@gmail.com>
2026-08-09 03:51:04 +0800

    chore(release): bump version to 0.13.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.caam.nothingelse"
         minSdk = 21
         targetSdk = 34
-        versionCode = 12
-        versionName = "0.12.0"
+        versionCode = 13
+        versionName = "0.13.0"
     }
 
     buildTypes {
